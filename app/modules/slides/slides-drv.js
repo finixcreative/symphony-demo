31eee5934d0858e49056e7a07eaeec2780e1d97c
finix-rsmith <rsmith@finixcreative.com>
2015-07-09 15:41:48 -0400

    Slides - set scope
    Set scope with "contents"

--- app/modules/slides/slides-drv.js
+++ app/modules/slides/slides-drv.js
@@ -12,16 +12,16 @@ angular.module('coreMod')
 			link: function (scope, elem, attrs){
 				scope.currentIndex = 0;
 				scope.next = function(){
-					scope.currentIndex < scope.slides.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
+					scope.currentIndex < scope.contents.slides.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
 				};
 				scope.prev = function(){
-					scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.slides.length - 1;
+					scope.currentIndex > 0 ? scope.currentIndex-- : scope.currentIndex = scope.contents.slides.length - 1;
 				};
 				scope.$watch('currentIndex', function() {
-					scope.slides.forEach(function(slide) {
+					scope.contents.slides.forEach(function(slide) {
 						slide.visible = false;
 					});
-					scope.slides[scope.currentIndex].visible = true;
+					scope.contents.slides[scope.currentIndex].visible = true;
 				});
 				var timer;
 				var sliderFunc = function() {

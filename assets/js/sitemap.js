083bad064bf48756aaedc7b8c88930d9fff8b540
finix-rsmith <rsmith@finixcreative.com>
2015-07-09 13:49:51 -0400

    Sitemap - model & controller
    Build sitemap database and controller, add scripts to index

---
+++ assets/js/sitemap.js
@@ -0,0 +1,56 @@
+var sitemap = {
+	pages: [
+		{
+			title: 'shop',
+			link: '/shop',
+		},
+		{
+			title: 'our story',
+			link: '/our-story',
+		},
+		{
+			title: 'blog',
+			link: '/blog',
+		},
+		{
+			title: 'help',
+			link: '/help',
+		},
+		{
+			title: 'contact',
+			link: '/contact',
+		},
+	],
+	subnavs: [
+		{
+			title: 'About Us',
+			sublinks: [
+				{title: 'Shipping & Delivery', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+			],
+		},
+		{
+			title: '',
+			sublinks: [
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+			],
+		},
+		{
+			title: '',
+			sublinks: [
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+				{title: '', link: '#',},
+			],
+		},
+	],
+};
